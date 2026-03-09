ca0c8894d2bc03df2b2497c87aeb5e39df1b84c0
@aquamore223 <emmyhollarhollar@gmail.com>
2026-03-09 08:38:50 -0700

    commit

--- js/index.js
+++ js/index.js
@@ -159,3 +159,38 @@ if (plus && minus && input) {
         }
     };
 }
+
+// target date
+const targetDate = new Date("Dec 31, 2026 23:59:59").getTime();
+
+const countdown = setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60)) / (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60)) / 1000
+    );
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.querySelector(".countdown").innerHTML = "EXPIRED";
+    }
+
+}, 1000);
